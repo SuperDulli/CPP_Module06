bfa34ef63fb0e6a89e93513d9ec148fe12a84e6b
chelmerd <chelmerd@student.42wolfsburg.de>
2022-07-01 13:31:33 +0200

    ex00: cleanup

--- ex00/convert.cpp
+++ ex00/convert.cpp
@@ -13,27 +13,9 @@ int main(int argc, char* argv[]) {
 	}
 	std::string const input = argv[1];
 	ft_literal::type const typeOfInput = ft_literal::detectType(input);
-	// std::cout << typeOfInput << std::endl;
-
-	// std::cout << (int) std::numeric_limits<char>::min() << std::endl;
-	// std::cout << (int) std::numeric_limits<char>::max() << std::endl;
-	// std::cout << std::numeric_limits<int>::min() << std::endl;
-	// std::cout << std::numeric_limits<int>::max() << std::endl;
-	// std::cout << std::numeric_limits<float>::min() << std::endl;
-	// std::cout << std::numeric_limits<float>::max() << std::endl;
-	// std::cout << std::setprecision(9) << static_cast<float>(std::numeric_limits<int>::max()) << std::endl;
-	// std::cout << std::numeric_limits<double>::max() << std::endl;
 
 	switch (typeOfInput)
-	{
-	case ft_literal::UNKOWN:
-		std::cerr
-		<< "Unkown input literal: '" << input
-		<< "'. Supported types are char, int, float (with -inff, +inff, nanf)"
-		<< " and double (with -inf, +inf, nan)"
-		<< std::endl;
-		return 1;
-		break;
+	{		
 	case ft_literal::CHAR:
 		ft_literal::convert(input.front());
 		break;
@@ -65,33 +47,39 @@ int main(int argc, char* argv[]) {
 		ft_literal::convert(-std::numeric_limits<double>::infinity());
 		break;
 	default:
+		std::cerr
+		<< "Unkown input literal: '" << input
+		<< "'. Supported types are char, int, float (with -inff, +inff, nanf)"
+		<< " and double (with -inf, +inf, nan)"
+		<< std::endl;
+		return 1;
 		break;
 	}
 	return 0;
 }
 
-void showError(std::string const& type, std::string msg) {
+void ft_literal::showError(std::string const& type, std::string msg) {
 	std::cout << type << ": " << msg << std::endl;
 }
 
-void showChar(char c) {
+void ft_literal::showChar(char c) {
 	if (!isprint(c)) {
-		showError("char", "Non displayable");
+		ft_literal::showError("char", "Non displayable");
 		return;
 	}
 	std::cout << "char: '" << c << "'" << std::endl;
 }
 
-void showInt(int i) {
+void ft_literal::showInt(int i) {
 	std::cout << "int: " << i << std::endl;
 }
 
-void showFloat(float f) {
+void ft_literal::showFloat(float f) {
 	std::cout << "float: " << std::showpoint << f << "f" << std::endl;
 	std::cout << std::noshowpoint;
 }
 
-void showDouble(double d) {
+void ft_literal::showDouble(double d) {
 	std::cout << "double: " << std::showpoint << d << std::endl;
 	std::cout << std::noshowpoint;
 }
@@ -110,29 +98,30 @@ ft_literal::type ft_literal::detectType(std::string const& s) {
 	if (s == "+inf") return ft_literal::INF_DOUBLE;
 
 	size_t i = 0;
-	while (i < s.length() && isspace(s.at(i))) i++; 
+	while (i < s.length() && isspace(s.at(i))) i++; // ignore leading whitespaces
 	if (i == s.length()) return ft_literal::UNKOWN;
-	if (s.at(i) == '+' || s.at(i) == '-') i++;
+	if (s.at(i) == '+' || s.at(i) == '-') i++; // optional sign
 	if (i == s.length()) return ft_literal::UNKOWN;
-	while (i < s.length() && isdigit(s.at(i))) i++;
-	if (i == s.length())
-		return ft_literal::INT;
+	while (i < s.length() && isdigit(s.at(i))) i++; // digit sequence
+	if (i == s.length()) return ft_literal::INT;
 	if (s.at(i) == '.') {
 		i++;
-		if (i == s.length()) return ft_literal::DOUBLE;
-		while (i < s.length() && isdigit(s.at(i))) i++;
-		if (i == s.length()) return ft_literal::DOUBLE;
-		if (s.at(i) == 'e' || s.at(i) == 'E') {
+		if (i == s.length()) return ft_literal::DOUBLE; // no suffix
+		while (i < s.length() && isdigit(s.at(i))) i++; // digit sequence
+		if (i == s.length()) return ft_literal::DOUBLE; // no suffix
+		if (s.at(i) == 'e' || s.at(i) == 'E') { // exponent
 			i++;
-			if (s.at(i) == '+' || s.at(i) == '-') i++;
+			if (s.at(i) == '+' || s.at(i) == '-') i++; // optional sign
 			if (i == s.length()) return ft_literal::UNKOWN;
-			while (i < s.length() && isdigit(s.at(i))) i++;
+			while (i < s.length() && isdigit(s.at(i))) i++; // digit sequence
 			if (i == s.length()) return ft_literal::DOUBLE;
 		}
-		if (isalpha(s.at(i) && s.at(i) != 'f' && s.at(i) != 'F')) return ft_literal::UNKOWN;
-		while (i < s.length() && isdigit(s.at(i))) i++;
-		if (i == s.length()) return ft_literal::DOUBLE;
-		if (i == s.length() - 1 && s.at(i) == 'f') return ft_literal::FLOAT;
+		if (isalpha(s.at(i) && s.at(i) != 'f' && s.at(i) != 'F')) // unkown suffix
+			return ft_literal::UNKOWN;
+		while (i < s.length() && isdigit(s.at(i))) i++; // digit sequence
+		if (i == s.length()) return ft_literal::DOUBLE; // no suffix
+		if (i == s.length() - 1 && (s.at(i) == 'f' || s.at(i) == 'F')) // float suffix
+			return ft_literal::FLOAT;
 	}
 	return ft_literal::UNKOWN;
 }
@@ -142,65 +131,65 @@ void ft_literal::convert(char val) {
 	int const		i = static_cast<int>(val);
 	float const		f = static_cast<float>(val);
 	double const	d = static_cast<double>(val);
-	showChar(c);
-	showInt(i);
-	showFloat(f);
-	showDouble(d);
+	ft_literal::showChar(c);
+	ft_literal::showInt(i);
+	ft_literal::showFloat(f);
+	ft_literal::showDouble(d);
 }
 
 void ft_literal::convert(int val) {
 	// char
 	if (val < std::numeric_limits<char>::max()
 		&& val > std::numeric_limits<char>::min()) {
-		showChar(static_cast<char>(val));
+		ft_literal::showChar(static_cast<char>(val));
 	} else {
-		showError("char", "impossible");
+		ft_literal::showError("char", "impossible");
 	}	
-	showInt(static_cast<int>(val));
-	showFloat(static_cast<float>(val));
-	showDouble(static_cast<double>(val));
+	ft_literal::showInt(static_cast<int>(val));
+	ft_literal::showFloat(static_cast<float>(val));
+	ft_literal::showDouble(static_cast<double>(val));
 }
 
 void ft_literal::convert(float val) {
 	// char
 	if (val < std::numeric_limits<char>::max()
 		&& val > std::numeric_limits<char>::min()) {
-		showChar(static_cast<char>(val));
+		ft_literal::showChar(static_cast<char>(val));
 	} else {
-		showError("char", "impossible");
+		ft_literal::showError("char", "impossible");
 	}
 	// int
 	if (val < static_cast<float>(std::numeric_limits<int>::max())
 		&& val > static_cast<float>(std::numeric_limits<int>::min())) {
-		showInt(static_cast<int>(val));
+		ft_literal::showInt(static_cast<int>(val));
 	} else {
-		showError("int", "impossible");
+		ft_literal::showError("int", "impossible");
 	}
-	showFloat(static_cast<float>(val));
-	showDouble(static_cast<double>(val));
+	ft_literal::showFloat(static_cast<float>(val));
+	ft_literal::showDouble(static_cast<double>(val));
 }
 
 void ft_literal::convert(double val) {
 	// char
 	if (val < std::numeric_limits<char>::max()
 		&& val > std::numeric_limits<char>::min()) {
-		showChar(static_cast<char>(val));
+		ft_literal::showChar(static_cast<char>(val));
 	} else {
-		showError("char", "impossible");
+		ft_literal::showError("char", "impossible");
 	}
 	// int
 	if (val < static_cast<float>(std::numeric_limits<int>::max())
 		&& val > static_cast<float>(std::numeric_limits<int>::min())) {
-		showInt(static_cast<int>(val));
+		ft_literal::showInt(static_cast<int>(val));
 	} else {
-		showError("int", "impossible");
+		ft_literal::showError("int", "impossible");
 	}
 	// float
 	if (val < static_cast<double>(std::numeric_limits<float>::max())
 		&& val > static_cast<double>(std::numeric_limits<float>::min())) {
-		showFloat(static_cast<float>(val));
+		ft_literal::showFloat(static_cast<float>(val));
 	} else {
-		showError("float", "impossible");
+		ft_literal::showError("float", "impossible");
 	}
-	showDouble(static_cast<double>(val));
+	ft_literal::showDouble(static_cast<double>(val));
 }
--- ex00/convert.hpp
+++ ex00/convert.hpp
@@ -18,12 +18,25 @@ namespace ft_literal
 		INF_DOUBLE,
 		N_INF_DOUBLE,
 	};
+
+	// reads the string and returns a number to indicate the type of the literal
 	type detectType(std::string const& s);
+
+	// convert will cast val into all the other types if possible
+
 	void convert(char val);
 	void convert(int val);
 	void convert(float val);
 	void convert(double val);
-} // namespace literal
 
+	// display results
+
+	void showError(std::string const& type, std::string msg);
+	void showChar(char c);
+	void showInt(int i);
+	void showFloat(float f);
+	void showDouble(double d);
+
+} // namespace literal
 
 #endif
